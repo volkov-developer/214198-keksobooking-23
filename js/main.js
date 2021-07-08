8be3b3bca5582b69494a21f867d29f68f61dec35
volkov-developer <sergey.volkov.freelancer@gmail.com>
2021-07-08 12:59:21 +0300

    Добавил переключение состояний форм

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ import { renderCard } from './card.js';
 import { enableForms, disableForms } from './form.js';
 
 disableForms();
-setTimeout(enableForms, 1500);
+setTimeout(enableForms, 2500);
 
 const ads = getAds();
 renderCard(ads[0]);
